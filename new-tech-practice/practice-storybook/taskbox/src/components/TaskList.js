e10428b4bb638047e7bc824595d44869641a6dd6
aosjehdgus <kdhsea@gmail.com>
2022-01-24 15:01:22 +0900

    Change task background to blue

--- new-tech-practice/practice-storybook/taskbox/src/components/TaskList.js
+++ new-tech-practice/practice-storybook/taskbox/src/components/TaskList.js
@@ -75,6 +75,8 @@ PureTaskList.defaultProps = {
   loading: false,
 };
 
+console.log('test');
+
 export function TaskList() {
   // We're retrieving our state from the store
   const tasks = useSelector((state) => state.tasks);
