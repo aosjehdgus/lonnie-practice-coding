9342be60b4963c0fd4ed5228a2a49d2614130baf
aosjehdgus <kdhsea@gmail.com>
2022-01-24 11:06:14 +0900

    Initial setting of storybook

--- new-tech-practice/practice-storybook/taskbox/.storybook/main.js
+++ new-tech-practice/practice-storybook/taskbox/.storybook/main.js
@@ -1,14 +1,10 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  // staticDirs: ['../public'],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
-  },
 };
