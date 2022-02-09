18a5fffe5254543d7a0b38fb90bd52e178c1e486
aosjehdgus <kdhsea@gmail.com>
2022-02-09 14:35:51 +0900

    Add todo list

--- react-typescript/basic-tech/src/App.tsx
+++ react-typescript/basic-tech/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoList } from './TodoList';
+import { TodoListItem } from './TodoListItem';
 
-function App() {
+const initialTodos: Array<Todo> = [
+  {text: "Walk the dog", complete: true},
+  {text: "Write app", complete: false}
+]
+
+const App: React.FC = () =>{
+  const [todos, setTodos] = useState(initialTodos)
+  
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo =>{
+      if (todo === selectedTodo){
+        return{
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo;
+    })
+    setTodos(newTodos);
+  }
+
   return (
-    <div>
-      Hello World
-    </div>
-  );
+  <>
+   <TodoList todos={todos} toggleTodo={toggleTodo} />
+  </>
+  )
 }
 
 export default App;
