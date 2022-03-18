644e0226bb7b0bea26b034ec77f70cbe5039fe93
aosjehdgus <kdhsea@gmail.com>
2022-03-18 18:45:34 +0900

    Solve the question

--- algorithm-practice/hash/did-not-comlete-player.js
+++ algorithm-practice/hash/did-not-comlete-player.js
@@ -28,7 +28,39 @@ participant	completion	return
 "mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다. 
 */
 
-function solution(participant, completion) {
-  var answer = '';
+// Test case 1 //
+const participant = ["mislav", "mislav", "mislav", "ana"];
+const completion = ["mislav", "mislav", "mislav"];
+
+
+// Test case 2 //
+// const participant = ["mislav", "stanko", "mislav", "ana"];
+// const completion = ["stanko", "ana", "mislav"];
+
+function solution(participants, completions) {
+  let answer = '';
+  const obj = {};
+  for(const participant of participants){
+    if(!obj[participant]){
+      obj[participant] = 1;
+    } else{
+      obj[participant] += 1;
+    }
+  }
+
+  for(const completion of completions){
+    if(obj[completion]){
+      obj[completion] -= 1;
+    }
+  }
+
+  for(const participant of participants){
+    if(obj[participant] >= 1){
+      answer = participant;
+    }
+  }
+
   return answer;
 }
+
+console.log(solution(participant, completion))
